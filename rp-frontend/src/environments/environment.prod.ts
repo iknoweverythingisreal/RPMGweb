e9f20a1a37f55b3c42d3367fe41bec70b2a35489
iknoweverythingisreal <xWaSTxTlexNY@gmail.com>
2026-03-06 23:46:07 +0700

    Update production environment with Render URL

--- rp-frontend/src/environments/environment.prod.ts
+++ rp-frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     // Please replace this with your actual Render/Railway backend URL!
     // Example: 'https://my-backend-rpmedia.onrender.com'
-    apiUrl: 'YOUR_BACKEND_URL_HERE'
+    apiUrl: 'https://rpmg.onrender.com'
 };
